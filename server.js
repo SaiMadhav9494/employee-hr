364d6b13726f57a0f8352dd25a2771fb7cff3920
Sai Madhav Perumalla <sai.madhav@scroll.network>
2019-11-13 20:37:41 -0600

    Update

--- server.js
+++ server.js
@@ -95,7 +95,7 @@ app.post('/employees', function(req, res) {
 
 app.get('/*', function(req, res) {
     
-    res.sendFile(path.join(__dirname+'/dist/beeble/index.html'));
+    res.sendFile(path.join(__dirname+'/dist/datis/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
